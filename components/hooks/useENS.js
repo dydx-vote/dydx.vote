b9c1d0949fda03d2aae360f64011cbb0e6e0c370
Aryeh Greenberg <13561405+arr00@users.noreply.github.com>
2022-04-18 15:21:30 -0400

    use our provider for ens

--- components/hooks/useENS.js
+++ components/hooks/useENS.js
@@ -1,13 +1,15 @@
-import { getDefaultProvider } from "@ethersproject/providers";
+import { ethers } from "ethers";
+import { web3p } from "containers"; // Web3
 import { useEffect, useState } from "react";
 
 const useENS = (address) => {
   const [ensName, setENSName] = useState();
+  const { web3 } = web3p.useContainer();
 
   useEffect(() => {
     const resolveENS = async () => {
       if (address) {
-        const provider = getDefaultProvider();
+        const provider = new ethers.providers.Web3Provider(web3.currentProvider);
         const ensName = await provider.lookupAddress(address);
         setENSName(ensName);
       }
