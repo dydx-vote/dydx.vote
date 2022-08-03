8d7e63f9973c5c2d24e9bad05a7852aace68a3db
arr00 <13561405+arr00@users.noreply.github.com>
2022-08-03 16:51:03 -0400

    push end time

--- helpers/index.js
+++ helpers/index.js
@@ -257,7 +257,7 @@ const canVote = async (address, proposalId) => {
     if (
       !(
         currentBlock > proposal.startBlock &&
-        currentBlock < proposal.endBlock - 2400
+        currentBlock < proposal.endBlock - 1900
       ) ||
       proposal.canceled
     ) {
